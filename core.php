cc2172c297e03abeb2d942d73d235617ae1f57b6
Shelob9 <Shelob9@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-22 05:54:10 +0000

    2.0.0
    git-svn-id: https://plugins.svn.wordpress.org/caldera-form-metabox/tags/2.0.0@1203669 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ core.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: Caldera Custom Fields
+ * Plugin URI:  
+ * Description: Create custom fields with powerful conditionals and processors using a Caldera Forms as the metabox designer.
+ * Version:     2.0.0
+ * Author:      David Cramer for CalderaWP LLC
+ * Author URI:  
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+define( 'CCF_PATH',  plugin_dir_path( __FILE__ ) );
+define( 'CCF_URL',  plugin_dir_url( __FILE__ ) );
+define( 'CCF_VER', '2.0.0' );
+define( 'CCF_CORE',  __FILE__ );
+
+
+/**
+ * Load plugin
+ */
+add_action( 'init', 'cf_custom_fields_init' );
+function cf_custom_fields_init() {
+	include_once( CCF_PATH . '/includes/metabox.php' );
+	include_once( CCF_PATH . '/includes/to-post-type.php' );
+}
